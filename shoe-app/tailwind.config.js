6a91b2885e28cbdac94eb09710d0821c15bb5da2
Hunter <hunterbrennan15@gmail.com>
2023-07-20 18:02:54 -0400

    added info bubbles under hero img

--- shoe-app/tailwind.config.js
+++ shoe-app/tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'hero-color': "#dcdedd",
+        'teal-main': "#1a3639",
+        'yellow-main': "#e7d869",
       },
       backgroundImage: {
         'hero-pattern': "url('../assets/imgs/heroimg.jpg')",
