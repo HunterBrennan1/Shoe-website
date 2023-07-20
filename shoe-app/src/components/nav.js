6a3927597aae3b55ab42de240a8d19e0d5620b1d
Hunter <hunterbrennan15@gmail.com>
2023-07-20 18:15:23 -0400

    styled nav bar componenets, added logo img

--- shoe-app/src/components/nav.js
+++ shoe-app/src/components/nav.js
@@ -1,4 +1,4 @@
-
+import Logo from "../assets/imgs/logoshoe1.png";
 
 export const Nav = () => {
   return (
@@ -7,15 +7,18 @@ export const Nav = () => {
       <div class="flex-wrap place-items-center">
         <nav class="flex justify-between bg-white text-black w-screen">
           <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-            <a class="text-2xl font-bold font-heading" href="#">
-              Showee
-            </a>
+            <div className="flex gap-3 align-center ">
+              <img className="w-13 h-14 rounded-full border-4 border-yellow-main b " src={Logo}></img>
+              <a class="text-2xl font-bold font-heading relative top-2 " href="#">
+                ShoeVibe
+              </a>
+            </div>
 
             <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-              <li><a class="hover:text-gray-200" href="#">Home</a></li>
-              <li><a class="hover:text-gray-200" href="#">Catagory</a></li>
-              <li><a class="hover:text-gray-200" href="#">Collections</a></li>
-              <li><a class="hover:text-gray-200" href="#">Contact Us</a></li>
+              <li><a class="hover:text-gray-200 underline underline-offset-8 decoration-teal-main decoration-4" href="#">Home</a></li>
+              <li><a class="hover:text-gray-200 underline underline-offset-8 decoration-yellow-main decoration-4" href="#">Catagory</a></li>
+              <li><a class="hover:text-gray-200 underline underline-offset-8 decoration-teal-main decoration-4" href="#">Collections</a></li>
+              <li><a class="hover:text-gray-200 underline underline-offset-8 decoration-yellow-main decoration-4" href="#">Contact Us</a></li>
             </ul>
 
             <div class="hidden xl:flex items-center space-x-5 items-center">
@@ -24,7 +27,7 @@ export const Nav = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
               </a>
-              <a class="flex items-center hover:text-teal-200" href="#">
+              <a class="flex items-center hover:text-yellow-main" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
@@ -36,7 +39,7 @@ export const Nav = () => {
               </a>
 
               <a class="flex items-center hover:text-yellow-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-teal-main" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
               </a>
