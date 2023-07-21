c229cfa4f3d95064ba85f0a1994291d104ab9ea8
Hunter <hunterbrennan15@gmail.com>
2023-07-21 16:27:49 -0400

    added text over video

--- shoe-app/src/components/video.js
+++ shoe-app/src/components/video.js
@@ -3,8 +3,9 @@ import Running from '../assets/imgs/running.mp4';
 
 export const Video = () => {
   return (
-    <section>
-      <video loop muted autoPlay src={Running}></video>
+    <section className="relative">
+      <video className=" z-0" loop muted autoPlay src={Running}></video>
+      <h1 className="video_title flex absolute items-center top-0 mt-1/2 z-10 w-full justify-center text-5xl text-white font-bold text-center">"Stride with confidence, conquer every step. Embrace your journey, reach new heights. With Stride Gear by your side, greatness knows no limits."</h1>
     </section>
   )
 }
