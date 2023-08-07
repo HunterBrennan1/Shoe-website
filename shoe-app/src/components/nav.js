a7fa69fe9a64431fde841c626b7fcdb0ab51d6a5
Hunter <hunterbrennan15@gmail.com>
2023-08-07 15:45:11 -0400

    cart button in nav links to checkout

--- shoe-app/src/components/nav.js
+++ shoe-app/src/components/nav.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/imgs/logoshoe1.png";
 
 
@@ -34,7 +35,7 @@ export const Nav = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
               </a>
-              <a class="flex items-center hover:text-yellow-main" href="#">
+              <Link to="/checkout" class="flex items-center hover:text-yellow-main">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
@@ -43,7 +44,7 @@ export const Nav = () => {
                   <span class="relative inline-flex rounded-full h-3 w-3 bg-yellow-500">
                   </span>
                 </span>
-              </a>
+              </Link>
 
               <a class="flex items-center hover:text-yellow-200" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-teal-main" fill="none" viewBox="0 0 24 24" stroke="currentColor">
