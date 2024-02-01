e173b632e221b216f4dae8754e5884f451285a8b
Hunter <hunterbrennan15@gmail.com>
2024-02-01 18:47:48 -0500

    changed color of cart button to look better over footer

--- shoe-app/src/components/shop.js
+++ shoe-app/src/components/shop.js
@@ -145,8 +145,8 @@ export const Shop = ({ totalPrice, setTotalPrice, }) => {
         </div>
       </div>
       <div>
-        <button className="show-modal-button flex fixed bottom-10 left-10 z-40 bg-teal-main shadow-xl border-4 border-yellow-main rounded-full p-5 w-20 h-20" onClick={handleButtonClick}>
-          <svg className='absolute w-10 left-4 bottom-4' xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white">
+        <button className="show-modal-button flex fixed bottom-10 left-10 z-40 bg-black-main shadow-xl border-4 border-black rounded-full p-5 w-20 h-20" onClick={handleButtonClick}>
+          <svg className='absolute w-10 left-4 bottom-5 top-4' xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
 
